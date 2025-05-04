29fe84e1e53aca57cb74e60e192ae9a5a5e8b236
Mostafa Wagih <mostafa.wagih@archerirm.com>
2025-05-04 18:10:21 +0300

    initial render for front end

--- back-end/index.js
+++ back-end/index.js
@@ -1,8 +1,10 @@
 const express = require("express");
+const cors = require("cors");
 const app = express();
-const PORT = 3000;
+const PORT = 3001;
 
 const itemsRoute = require("./routes/items");
+app.use(cors());
 
 app.use(express.json());
 app.use("/api/items", itemsRoute);
